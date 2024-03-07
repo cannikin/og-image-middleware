ce2b8e0d7fcb4362cbbc0ba1229ed7d631f43f05
Rob Cameron <cannikin@fastmail.com>
2024-03-06 20:53:13 -0800

    Format on save, adds title above image

--- web/src/pages/HomePage/HomePage.jpg.jsx
+++ web/src/pages/HomePage/HomePage.jpg.jsx
@@ -2,26 +2,41 @@ import { useLocation } from '@redwoodjs/router'
 // import photo from './assets/photo.jpg'
 
 export const data = async () => {
-  return { Date: 'Jun 3 2023 11:11am', Copyright: 'Rob Cameron', Camera: 'Fujifilm XT-1', Lens: '16mm', ISO: 200, Aperture: 'f/9', Shutter: '1/350' }
+  return {
+    Date: 'Jun 3 2023 11:11am',
+    Copyright: 'Rob Cameron',
+    Camera: 'Fujifilm XT-1',
+    Lens: '16mm',
+    ISO: 200,
+    Aperture: 'f/9',
+    Shutter: '1/350',
+  }
 }
 
 export const output = ({ data }) => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const { searchParams } = useLocation()
+  const { origin, searchParams } = useLocation()
   const query = Object.fromEntries(searchParams.entries())
 
   return (
-    <div className="p-8 bg-gray-200 h-full">
-      <div className="p-8 bg-white rounded-lg shadow-lg">
+    <div className="h-full bg-gray-200 p-8">
+      <div className="rounded-lg bg-white p-8 shadow-lg">
         <div className="flex items-start space-x-8">
-          <div className="w-8/12">
-            <img src="http://localhost:8910/photo.jpg" className="w-full shadow rounded"/>
+          <div className="relative w-8/12">
+            <img
+              src={`${origin}/photo.jpg`}
+              className="w-full rounded shadow"
+              alt="blah"
+            />
+            <h1 className="absolute inset-x-0 top-4 text-center text-5xl font-semibold text-white">
+              OG Image Middleware!
+            </h1>
           </div>
 
           <div className="w-4/12">
             <table className="metadata">
               <tbody>
-                {Object.entries(data).map(([key,value]) => (
+                {Object.entries(data).map(([key, value]) => (
                   <tr key={key}>
                     <td>
                       <label>{key}</label>
