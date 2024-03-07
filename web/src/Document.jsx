9c5f63c5a9d376f28481e16d2231ffe55608d30b
Rob Cameron <cannikin@fastmail.com>
2024-03-06 20:53:26 -0800

    Formatting on save

--- web/src/Document.jsx
+++ web/src/Document.jsx
@@ -10,8 +10,15 @@ export const Document = ({ children, css, meta }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" type="image/png" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin={true}
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
         <Css css={css} />
         <Meta tags={meta} />
       </head>
